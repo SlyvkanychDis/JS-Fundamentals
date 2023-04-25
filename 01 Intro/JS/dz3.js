c1c569bd31a12021d726f1b7ce47e848b7aecd72
DIS01~ <slvdimaslv@gmail.com>
2023-04-25 12:03:29 +0300

    task 4 (vesion 1 and version 2)

--- 01 Intro/JS/dz3.js
+++ 01 Intro/JS/dz3.js
@@ -30,14 +30,38 @@ console.log(sequenceNumbers(1,6));*/
 
 /*3.Напишіть функцію randArray(k), яка заповнюватиме масив k випадковими 
 цілими числами. Випадкові числа генеруються із діапазону 1-500.*/
-function randArray() {
+/*function randArray(k) {
     const res2 = [];
-    for (let n = 1; n <= 20; n++){
+    for (let k = 1; k <= 20; k++){
    res2.push(Math.floor(Math.random() * (500 - 1) + 1));
     }
     return res2;
  }
-console.log(randArray());
+console.log(randArray());*/
+
+/*4.Написати функцію compact() яка має приймати на вхід масив, а на вихід має 
+повертати значення нового масиву без повторень.
+приклад:
+const arr = [5, 3, 4, 5,6,7,3];
+const arr2 = compact(arr);
+console.log(arr2) ; // [5,3,4,6,7]*/
+/*const arr = [5,3,4,5,6,7,3];
+const arr2 = compact(arr);
+function compact (arr){
+  const arr2 = [];
+  const tempArray =[...arr].sort();
+  for (let i = 0; i<tempArray.length; i++){
+    if (tempArray[i + 1] !== tempArray[i]){
+      arr2.push(tempArray[i]);
+    }
+  }return arr2;
+}
+console.log(arr2); *//*version 1*/
+
+
+/*const arr = [5, 3, 4, 5,6,7,3];
+const arr2 = [...new Set(arr)];
+console.log(arr2);*//*version 2*/
 
 /*8.
 let firstArray = [1, 2, 3, 5, 3];
