0805a816eeaf3b5e13649580e5bdd939e3891cd9
DIS01~ <slvdimaslv@gmail.com>
2023-05-03 23:59:45 +0300

    3 task

--- 01 Intro/JS/dz6_dom.js
+++ 01 Intro/JS/dz6_dom.js
@@ -24,3 +24,33 @@ const third = elements[2].textContent;
 const result = `${first}, ${last}, ${second}, ${fourth}, ${third}`;
 
 alert(result);*/
+
+/*2.
+Для сторінки
+<body>
+  .....
+</body>
+Напишіть скріпт, який за допомогою засобів DOM простилізує сторінку так як показано на картинці.*/
+
+
+/*3. 
+Напишіть скріпт, який за допомогою засобів DOM створить для порожньої HTML-сторінки таку структуру з тегів і їх атрибутів.
+<body>
+  main class="mainClass check item">         
+     <div id="myDiv">
+         <p>First paragraph</p>           
+     </div>
+ </main> 
+</body>*/
+/*
+const mainElement = document.createElement("main");
+mainElement.setAttribute("class", "mainClass check item");
+const divElement = document.createElement("div");
+divElement.setAttribute("id", "myDiv");
+const pElement = document.createElement("p");
+const pText = document.createTextNode("First paragraph");
+pElement.appendChild(pText);
+divElement.appendChild(pElement);
+mainElement.appendChild(divElement);
+document.body.appendChild(mainElement);*/
+
