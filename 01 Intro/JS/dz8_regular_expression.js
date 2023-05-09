b5d432991f934aea36bf375d930891f8bf2f6d9a
DIS01~ <slvdimaslv@gmail.com>
2023-05-09 14:12:42 +0300

    tasks 3, 5

--- 01 Intro/JS/dz8_regular_expression.js
+++ 01 Intro/JS/dz8_regular_expression.js
@@ -18,3 +18,54 @@ function checkEmail(email) {
     return regex.test(email);
   }
 console.log(checkEmail("Qmail@gmail.com"));
+
+/*3.Напишіть регулярний вираз, який в рядковому тексті 2 підстрічки буде міняти місцями.
+Приклад роботи:
+Вхідний рядок: "Java Script"
+Вихід: “Script, Java”*/
+
+let inputString = "Java Script";
+let regex = /^(.*?)\b(\w+)\b(.*?\b)(\w+)\b(.*?)$/;
+let replacementPattern = "$4$3$2$1$5";
+let result = inputString.replace(regex, replacementPattern);
+console.log(result);
+
+/*4.Напишіть функцію, яка виконуватиме валідацію номера банківської карти (9999-9999-9999-9999).*/
+
+
+
+/*5.Напишіть функцію, яка приймає рядкові дані і виконує перевірку на їх відповідність емейлу.
+Вимоги:
+ •  Цифри (0-9).
+ •  Тільки латинські літери в великому (A-Z) і малому (a-z) регістрах.
+ •  В тілі емейла допустимі лишеі символи “_” і “-”. Але вони не можуть бути 1-им символом емейлу.
+ •  Символ “-” не може повторюватися.
+checkEmail('my_mail@gmail.com');
+"Email is correct!"
+checkEmail('#my_mail@gmail.com');
+"Email is not correct!"
+checkEmail('my_ma--il@gmail.com');
+"Email is not correct!"*/
+
+function checkEmail(email) {
+  const pattern = /^[A-Za-z0-9]+([_\-.][A-Za-z0-9]+)*@[A-Za-z0-9]+([\-][A-Za-z0-9]+)*(\.[A-Za-z]{2,}){1,2}$/;
+  if (pattern.test(email)) {
+    return "Email is correct!";
+  } else {
+    return "Email is not correct!";
+  }
+}
+console.log(checkEmail('my_mail@gmail.com'));
+console.log(checkEmail('#my_mail@gmail.com'));
+console.log(checkEmail('my_ma--il@gmail.com'));
+
+/*6.6. 
+Напишіть функцію, яка перевіряє правильність логіна. Правильний логін - рядок від 2 до 10 символів, що містить лише букви та числа, номер не може бути першим. Функція має приймати рядок і знаходити усі числа в цьому рядку, включаючи числа з плаваючою комою (наприклад, 3.4).
+Приклад роботи:
+checkLogin('ee1.1ret3');
+true 
+//1.1, 3
+checkLogin('ee1*1ret3');
+false 
+//1, 1, 3*/
+
