c064b2963e7f6b4b4bd5effe5cc14f63f83d6349
DIS01~ <slvdimaslv@gmail.com>
2023-05-16 10:31:21 +0300

    task 1

---
+++ 01 Intro/JS/dz9_jquery.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    var headers = $("h2.head");
+      headers.css("background-color", "green");
+      var innerElements = headers.find(".inner");
+    innerElements.css("font-size", "35px");
+  });
