14be48f37d0d390f5775d8d9db170068b2f52819
DIS01~ <slvdimaslv@gmail.com>
2023-05-31 09:46:51 +0300

    tasks

--- 01 Intro/JS/dz13_ajax.js
+++ 01 Intro/JS/dz13_ajax.js
@@ -9,4 +9,25 @@ function makeRequest() {
     };
     xhr.open("GET", "server.php", true); 
     xhr.send(); 
+  };
+
+
+  function makeRequest() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var response = xhr.responseText; 
+        var books = JSON.parse(response);
+        var authorsList = document.getElementById("authorsList");
+        authorsList.innerHTML = "";
+        for (var i = 0; i < books.length; i++) {
+          var author = books[i].author;
+          var listItem = document.createElement("li");
+          listItem.innerHTML = author;
+          authorsList.appendChild(listItem);
+        }
+      }
+    };
+    xhr.open("GET", "books.json", true);
+    xhr.send(); 
   }
